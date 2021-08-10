0b09915f5a0b5a76c2815340ada5883ac48e3ef2
TristanOrta <oscar_tristan_13@hotmail.com>
2021-08-10 18:20:03 -0500

    first commit

---
+++ database/migrations/2021_08_10_034854_empleados.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Empleados extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('nombre');
+            $table->string('correo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('alumnos');
+    }
+}
